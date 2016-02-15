c76c208318617b7e91040dd6ae4742af0a37793c
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-02-15 18:29:08 -0200

    Update package info

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
-  name: 'trendyshop:creditcardform',
+  name: 'astrocoders:creditcardform',
   version: '1.1.0',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Add jessepollak card lib to client',
+  git: 'https://github.com/Astrocoders/meteor-credit-card-form',
   documentation: 'README.md'
 });
 
